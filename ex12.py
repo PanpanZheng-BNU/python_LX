f6f8619b43fefca87e77b6d2b66de7038dd1927b
Your Name <email@example.com>
2019-10-18 17:49:14 +0800

    2

--- ex12.py
+++ ex12.py
@@ -3,3 +3,5 @@ height = input("How tall are you? ")
 weight = input("How much do you weight? ")
 
 print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+# It could run successfully, but couldn't save the input as a variable
+print("How old are you?", input())
